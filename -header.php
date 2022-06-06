1cdbc5e63ec7062eadb393b21cb778aa702f5246
Ana Luiza <yanaluiza26y@gmail.com>
2022-06-05 22:28:53 -0300

    Delete -header.php

--- -header.php
+++
@@ -1,9 +0,0 @@
-  <header> 
-    <nav>
-      <ul>
-        <li><a href="index.php">HOME</a></li>
-        <li><a href="produtos.php">PRODUTOS</a></li>
-        <li><a href="contatos.php">CONTATO</a></li>
-      </ul>
-    </nav>
-  </header>
