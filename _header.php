89a5e9c5f86eedb6e079ad4ecc43ad5833db337f
Ana Luiza <yanaluiza26y@gmail.com>
2022-06-10 12:01:22 -0300

    Add files via upload

---
+++ _header.php
@@ -0,0 +1,18 @@
+<header>
+
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Ativo</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#">Desativado</a>
+  </li>
+</ul>
+
+</header>
