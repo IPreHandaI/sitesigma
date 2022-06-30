720da89085836bbd3a78ca2cf9349ba1e0e67e05
Ana Luiza <yanaluiza26y@gmail.com>
2022-06-30 19:37:54 -0300

    06/30/2022

---
+++ contato.php
@@ -0,0 +1,34 @@
+<?php 
+
+include_once './includes/_head.php';
+include_once './includes/_dados.php';
+include_once './includes/_header.php';
+
+
+if (isset( $_POST['txtNome'])) {
+    $nome = strtoupper($_POST['txtNome'] ); 
+    $a = strtoupper($_POST['txttlf'] ); 
+    $b = strtoupper($_POST['txtemail'] ); 
+    $c = strtoupper($_POST['txtsenha'] ); 
+    echo $nome . "<br>";
+    echo $a . "<br>";
+    echo $b . "<br>";
+    echo $c . "<br>";
+}
+
+?>
+
+<h1>Contato</h1>
+
+<form action="./contato.php" method="post">     
+    <input class="txtNome" type="text" name="txtNome" id="txtNome" placeholder="Nome Completo">
+    <input class="txtNome" type="tel" name="txttlf" id="txttlf" placeholder="Número de telefone">
+    <input class="txtNome" type="email" name="txtemail" id="txtemail" placeholder="E-mail" required>
+    <textarea name="txtemail" id="" cols="30" rows="10"></textarea>
+    <input class="txtNome" type="password" name="txtsenha" id="txtsenha" placeholder="Senha">
+    <input class="botao" type="submit" value="cadastrar">
+</form>
+
+<?php 
+include_once './includes/_footer.php';
+?>
---
+++ produtos.php
@@ -0,0 +1,33 @@
+<?php
+include_once './includes/_head.php';
+include_once './includes/_header.php';
+include_once './includes/_dados.php';
+?>
+
+<div class="container">
+    <h2 class="clear">Promoção</h2>
+    <div class="row mt-5">
+    
+        <?php 
+            for ($i=0; $i <= 30; $i++) { 
+        
+        ?>
+        <div class="card m-3" style="width: 18rem;">
+        <img src="./imgs/<?php echo $produtos[$i]['imagem'];?>" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title"><?php echo $produtos[$i]['nome'];?></h5>
+            <p class="card-text"><?php echo $produtos[$i]['descrição'];?></p>
+            <a href="produtos.php?id=<?php echo $i;?>&tipo=promocao" class="btn btn-primary">Comprar</a>
+            </div>
+        </div>
+        <?php
+            }
+        ?>
+    </div>
+</div>
+
+<?php
+
+include_once './includes/_footer.php'
+
+?>
